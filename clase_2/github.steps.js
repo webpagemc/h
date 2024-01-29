0a79d75db5de67ed591d220c5a69c99d2de36a57
mauklo <maucalodolce20@gmail.com>
2024-01-29 19:10:35 -0300

    add commands

---
+++ clase_2/github.steps.js
@@ -0,0 +1,60 @@
+"GitHub Comandos"
+
+//GIT CLONE
+
+//Clonar un repositorio remoto dentro de nuestra PC
+
+//GIT PULL
+
+//Actualizar un repositorio local, con los datos de uno remoto
+
+
+
+// ==== Ramas ===== //
+
+//GIT BRANCH
+
+//Ver las ramas del proyecto (y ver en que rama estas parado)
+
+//GIT BRANCH "NombreDeNuevaRama"
+
+//Crear una rama nueva
+
+//GIT CHECKOUT "NombreDeLaRama"
+
+//Moverse de una rama a otra
+
+
+
+// ==== ADD, COMMIT Y PUSH ==== //
+
+//GIT ADD "urlDeLosCambios/cambios"
+
+//Agregar un cambio
+
+//GIT ADD .
+
+//Agregar todos los cambios
+
+
+//GIT COMMIT -m "Mensaje"
+
+//Confirmar los cambios
+
+
+//GIT PUSH
+
+//Subir los cambios al repositorio remoto
+
+//("En caso de trabajar en otra rama se usa:  git push --set-upstream origin NombreDeLaRama  ")
+
+
+//==== correcciones ====//
+
+//GIT RESTORE "urlDeLosCambios/cambios"
+
+//GIT RESTORE .
+
+//Sirve para cancelar los GIT ADD.
+
+
