37cbcd3e1749c0ce435d6e40d3b90ed28b0a01fa
mauklo <maucalodolce20@gmail.com>
2024-01-31 21:39:04 -0300

    add activities & code comments

---
+++ actividades/clase_3.js
@@ -0,0 +1,23 @@
+// Datos de los productos y sus precios
+const productos = [
+    { id: 1, nombre: 'Laptop', precio: 250000 },
+    { id: 2, nombre: 'Teléfono', precio: 100000 },
+    { id: 3, nombre: 'Tablet', precio: 110000 },
+    { id: 4, nombre: 'Smartwatch', precio: 75000 },
+    { id: 5, nombre: 'Auriculares', precio: 10000 }
+  ];
+
+//Create una funcion que haga cada una de las cosas:
+  
+// 1. Imprimi en la consola el nombre de cada producto junto con su precio.
+
+// 2. Devolve solo los productos que tienen un precio superior a 100000.
+  
+// 3. Calcula el precio total de todos los productos.
+  
+// 4. Agrega un nuevo producto.
+
+// 5. Crea una funcion en la que pases un id como parametro y esta te retorne el producto en cuestion
+//    si el producto no existe que imprima un mensaje
+
+  
