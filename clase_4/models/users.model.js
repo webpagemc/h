9d8ccb517e810a0926689ece1f06f256bd2d4a80
hernan <hamrahernan@gmail.com>
2024-02-07 19:25:02 -0300

    agregué modelos

--- clase_4/models/users.model.js
+++ clase_4/models/users.model.js
@@ -10,19 +10,78 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:true,
         unique:true
+    },
+    nombre:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    apellido:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    altura:{
+        type:Number,
+        required:true,
+        unique:true
+    },
+    peso:{
+        type:Number,
+        required:true,
+        unique:true
+    },
+    ciudad:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    zona_horario:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    test_fuerza:{// aca serian varios valores de fuerza de cada grupo muscular...
+        type:Number,
+        required:true,
+        unique:true
     }
+
 
 });
 
 const userModel = mongoose.model("users",userSchema );
 
 userModel.create({
-    username:"Mauro",
-    email:"mauro@mail.com"
+    username:"hernan",
+    email: "mauro@mail.com",
+    nombre: "Hernán",
+    apellido: "Hamra",
+    altura: 1.78,
+    peso: 89,
+    ciudad: "Buenos Aires", //aca deberia haber ciudad por defalutl con la banda horaria
+    zona_horario:"ut8", //aca deberia haber  por defalutl la banda horaria segun ciudad
+
 })
 
-{
-    username:"Mauro",
-    email:
-}
+const entrenamiento = new mongoose.Schema({
+    nombre:{ // habroia por default que elegir entre fuerza, resistencia, hipertrofia, trasnferencia deportiva.
+        type:String,
+        required:true
+    },
+    descripcion:{
+        type:String,
+        required:true
+    },
+    nivel:{//habria que elegir por default tres niveles- iniciado, intermedio, avanzado y pro
+        type:String,
+        required:true
+    },
+    intensidad:{
+        type:Number,
+        required:true
+    },
+
+
+})
 
